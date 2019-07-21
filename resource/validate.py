36bc2927ed64029e8122b8e2b74c162fc91dbac6
Advaith Narayan <42429987+bash-shocker@users.noreply.github.com>
2019-07-21 17:49:27 +0530

    Update validate.py

--- resource/validate.py
+++ resource/validate.py
@@ -1,6 +1,6 @@
 import re
 def check(roll): 
-	if re.match(r'[a-zA-Z]{3}[0-9]{2}[a-zA-Z]{2}[0]{1}[0-3]{1}[0-4]{1}$', roll):
+	if re.match(r'[a-zA-Z]{3}[0-9]{2}[a-zA-Z]{2}[0]{1}[0-3]{1}[0-9]{1}$', roll):
 		pass
 	else:
 		print("\n\n\tOOPS!! You've entered the wrong roll number\n\n")
